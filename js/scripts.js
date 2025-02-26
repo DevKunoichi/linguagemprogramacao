31430bba42a8459aaf84af67c53b371f9cdcea8f
DevKunoichi <andresa.paula1981@gmail.com>
2025-02-26 18:35:06 -0300

    Correções nos exercícios

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-function seguir(){
+function seguir() {
     alert(`Você agora está seguindo Daniel Pires`)
+}
+// exercicio1
+function exe1() {
+    // vamos usar DOM para recuerar os dados do HTML
+    // DOM - Document Object Model
+    // converte texto para número - Number()
+    let nro1 = Number (document.getElementById("nro1").value)
+    let nro2 = Number (document.getElementById("nro2").value)
+    // vamos fazer a subtração 
+    let sub = nro1 - nro2
+    // vamos mostrar o resultado ao usuário
+    // alert("A subtração é "+ sub)
+    document.getElementById("sub").innerText = "O resultado é: " + sub
 }
