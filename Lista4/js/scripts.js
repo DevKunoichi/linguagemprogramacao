f3dcc5e855b8c63ee49fad3be5e53f083d868b6d
ANDRESA NASCIMENTO DE PAULA <1090482421041@fatec-srv.local>
2025-04-29 09:13:49 -0300

    Refatoração dos exercícios 2 e 3 da Lista 4

--- Lista4/js/scripts.js
+++ Lista4/js/scripts.js
@@ -28,7 +28,7 @@ function exe2(){
         numeros.push(Number(prompt(`Informe o elemento ${i}`)))
     }
     //calcula números divisíveis por 2, 3, 2e3
-    let multiplos2 = [], multiplos3 = [], multiplos2e3 []
+    let multiplos2 = [], multiplos3 = [], multiplos2e3 = []
     for(let i=0; i<numeros.length; i++){
        if(numeros[i] % 2 == 0 && numeros[i] % 3 == 0){
         multiplos2e3.push(numeros[i])
@@ -41,4 +41,40 @@ function exe2(){
        }
     }
     alert(`Múltiplos de 2 ${multiplos2} Múltiplos de 3 ${multiplos3} e Múltiplos de 2 e 3 ${multiplos2e3}`)
+}
+//exercício3
+function exe3(){
+    //cadastra código e qtde em estoque
+    let codigos = [], estoque = [] //declara os vetores
+    for(let i=0; i<3; i++){
+        codigos.push(prompt(`Informe o código do produto`))
+        estoque.push(Number(prompt(`Informe a quantidade em estoque`)))
+    }
+    //comprando...
+    let cliente
+    do {
+        let cliente = Number(prompt(`Informe o código do cliente`))
+        let codigo = prompt(`Informe o código do produto para compra`)
+        let qtde = Number(prompt(`Informe a qtde do produto para compra`))
+        //procura para ver se o produto existe (ou é feito um 'for' procurando ou é feita uma 'função')
+        //o códigos é um vetor
+        let posicao = codigo.indexOf(codigo)
+        if (posicao == -1){
+            alert(`Código ${codigo} inexistente`)
+        }
+        else {//produto existe
+            //tem em estoque o suficiente
+            if (estoque[posicao]>=qtde){
+                //atualiza o estoque
+                estoque[posicao] = estoque[posicao] - qtde
+                alert(`Pedido atendido. Obrigado e volte sempre.`)
+            }
+            else{
+                alert(`Não temos estoque suficiente dessa mercadoria.`)
+            }
+        }
+        cliente = Number(prompt(`Informe código do novo cliente. Digite 0 para encerrar.`))
+    }//fecha o do
+    while(cliente != 0)
+    alert(`${codigos} ${estoque}`)
 }
