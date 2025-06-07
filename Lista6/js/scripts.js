48440612ced9b5d7862ff6913982a56c13d5e55d
DevKunoichi <andresa.paula1981@gmail.com>
2025-06-07 07:46:54 -0300

    Exercícios de Matriz

--- Lista6/js/scripts.js
+++ Lista6/js/scripts.js
@@ -1,5 +1,5 @@
-//função 1 
-function entrada(vet){ // vet = vetor
+//função1 
+function entrada(vet){ //vet = vetor
     vet[0] = 'A'
     vet[1] = 'B'
     vet[2] = 'C'
@@ -14,14 +14,14 @@ function saida(vet){
 }
 function main(){
     let vetor = []
-    // passando vetor como parâmetro da função
-    // vet vai receber uma referência de vetor (passagem de parâmetro por referência)
+    //passando vetor como parâmetro da função
+    //vet vai receber uma referência de vetor (passagem de parâmetro por referência)
     entrada(vetor) 
     processa(vetor)
     saida(vetor)
 }
 main()
-//função 2
+//função2
 function entrada(va){
     va = va + 10
 }
@@ -33,8 +33,8 @@ function saida(va){
 }
 function main(){
     let a = 10
-    // passando um número como parâmetro da função
-    // va vai receber o valor de a (passagem de parâmetro por valor)
+    //passando um número como parâmetro da função
+    //vai receber o valor de a (passagem de parâmetro por valor)
     entrada(a)
     processa(a)
     saida(a)
@@ -42,39 +42,39 @@ function main(){
 main()
 //exercício 3 utilizando 'função'
 function entrada(vet){
-    for(let i=0; i<3;i++){
-        // cria objeto
+    for(let i=0; i<3; i++){
+        //cria objeto
         let objeto = {
             codigo: prompt(`Informe o código do produto`),
             estoque: Number(prompt(`Informe a qtde em estoque`)),
             preco: Number(prompt(`Informe o preço do produto`))
         }
-        // adiciona objeto no vetor
+        //adiciona objeto no vetor
         vet.push(objeto)
     }
 }
 function compra(vet){
-        // comprando ...
+        //comprando ...
     let cliente = Number(prompt(`Informe código do cliente`))
     do {
         let codigo = prompt(`Informe código do produdo para compra`)
         let qtde = Number(prompt(`Informe a qtde do produto para compra`))
-        // procura pra ver se o produto existe
+        //procura pra ver se o produto existe
         let produtoEncontrado
         let i
-        for(i=0;i<3;i++){
-            if (vet[i].codigo == codigo){ // produtor encontrado
-                produtoEncontrado = vet[i] // guarda produto em produtoEncontrado
-                break // pára de procurar - sair do for
+        for(i=0; i<3; i++){
+            if (vet[i].codigo == codigo){ //produto encontrado
+                produtoEncontrado = vet[i] //guarda produto em produtoEncontrado
+                break //pára de procurar - sair do for
             }
         }
-        if (i == 3){ // produto não existe
+        if (i == 3){ //produto não existe
             alert(`Código ${codigo} inexistente`)
         }
-        else { // produto existe
-            // tem em estoque suficiente
+        else { //produto existe
+            //tem em estoque suficiente
             if ( produtoEncontrado.estoque >= qtde){
-                // atualiza o estoque
+                //atualiza o estoque
                 produtoEncontrado.estoque -= qtde
                 alert(`Pedido atendido. Obrigado e volte sempre`)
             }
@@ -83,16 +83,16 @@ function compra(vet){
             }
         }
         cliente = Number(prompt(`Informe código do novo cliente. Digite 0 para encerrar`))
-    } // fecha o do
+    } //fecha o do
     while (cliente != 0)
 }
 function saida(vet){
-    for(let i=0;i<vet.length;i++){
+    for(let i=0; i<vet.length; i++){
         alert(`Código ${vet[i].codigo} Estoque ${vet[i].estoque} Preco: ${vet[i].preco}`)
     }
 }
 function exe3(){
-    // cadastra código e qtde em estoque
+    //cadastra código e qtde em estoque
     let vetor = []
     entrada(vetor)
     compra(vetor)
@@ -100,44 +100,62 @@ function exe3(){
 }
 //exercício 6 utilizando 'função'
 function exe6(){
-    let vetor = []
-    // entrada de dados
-    for(let i=0;i<3;i++){
-        // cria o objeto
+    //vet recebe vetor (vet = vetor)
+    //entrada de dados
+    for(let i=0; i<3; i++){
+        //cria o objeto
         let obj = {
             nome: prompt(`Informe nome do ${i+1}o vendedor`),
             vendas: Number(prompt(`Informe total vendido pelo ${i+1}o vendedor`)),
             percentual: Number(prompt(`Informe percentual de vendas do ${i+1}o vendedor`))
         }
-        // calcula a comissão -> vamos utilizar o . para acessar o campo/atributo do objeto
+        //calcula a comissão -> vamos utilizar o . para acessar o campo/atributo do objeto
         obj.comissao = (obj.vendas * obj.percentual) / 100
-        // adiciona o objeto no vetor
+        //adiciona o objeto no vetor
         vetor.push(obj)
-    } // fecha o for
-
-    // relação dos vendedores e os valores a receber
+    } //fecha o for
+}
+function gerarRelatorio(vet){
+    //relação dos vendedores e os valores a receber
     let relatorio = ''
-    for(let i=0;i<3;i++){
-        relatorio += `\nVendedor: ${vetor[i].nome} vai receber ${vetor[i].comissao}`
+    for(let i=0; i<3; i++){
+        relatorio += `\nVendedor: ${vet[i].nome} vai receber ${vet[i].comissao}`
     }
     alert(relatorio)
-    // total das vendas de todos os vendedores
+}
+function totalVendas(vet){
+    //total das vendas de todos os vendedores
     let somaVendas = 0
-    for(let i=0;i<3;i++){
-        somaVendas = somaVendas + vetor[i].vendas
+    for(let i=0; i<3; i++){
+        somaVendas = somaVendas + vet[i].vendas
     }
     alert(`Total de vendas ${somaVendas.toFixed(2)}`)
-    // calcula o maior e menor valor de comissão
-    let objMaior = vetor[0] // primeiro funcionário é o maior
-    let objMenor = vetor[0] // primeiro funcionário é o menor
-    for(let i=1;i<3;i++){
-        if (vetor[i].comissao > objMaior.comissao){
-            objMaior = vetor[i]
-        }
-        if (vetor[i].comissao < objMenor.comissao){
-            objMenor = vetor[i]
-        }
-    }
-    alert(`${objMaior.nome} vai receber maior comissão de ${objMaior.comissao}`)
-    alert(`${objMenor.nome} vai receber menor comissão de ${objMenor.comissao}`)
 }
+function menorMaiorComissao(vet){
+    // calcula o maior e menor valor de comissão
+    let objMaior = vet[0] // primeiro funcionário é o maior
+    let objMenor = vet[0] // primeiro funcionário é o menor
+    for(let i=1; i<3; i++){
+        if (vet[i].comissao > objMaior.comissao){
+            objMaior = vet[i]
+            }
+            if (vet[i].comissao < objMenor.comissao){
+            objMenor = vet[i]
+            }
+        }
+        alert(`${objMaior.nome} vai receber maior comissão de ${objMaior.comissao}`)
+        alert(`${objMenor.nome} vai receber menor comissão de ${objMenor.comissao}`)
+}
+function exe6(){
+    let vetor = []
+    entrada(vetor)
+    gerarRelatorio(vetor)
+    totalVendas(vetor)
+    menorMaiorComissao(vetor)
+}
+exe6()
+
+   
+    
+    
+    
---
+++ Lista7/js/scripts.js
@@ -0,0 +1,65 @@
+//exemplo1
+function entrada(mat){
+    //será 3 x 3
+    for(let i=0; i<3; i++){
+        mat[i] = [] //cria cada linha da matriz
+        for(let j=0; j<3; j++){
+            mat[i][j] = Number(prompt(`Informe o elemento [${i}][${j}] da matriz.`))
+        }
+    }
+}
+function calculaSomaDiagonalPrincipal(mat){
+    let soma = 0
+    for(let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+           if (i == j){
+                soma += mat[i][j]
+            }
+        }
+    }
+    return soma
+}
+function exemplo(){
+    let mat = []
+    entrada(mat)
+    alert(`A soma da diagonal principal é ${calculaSomaDiagonalPrincipal(mat)}`)
+}
+exemplo()
+
+//exercício1
+function exe1(mat){
+    for (let i=0; i<3; i++){
+         mat[i] = []
+         for(let j=0; j<3; j++){
+            mat[i][j] = parseInt(Math.random() * 10) + 15 //gera número aletório 
+         }
+    }
+}
+function somaEntre15e20(mat){
+    let soma = 0
+    for(let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+            if(mat[i][j] >= 15 && mat[i][j] <= 20){
+                soma++
+            }
+        }
+    }
+    return soma
+}
+function exibeMatriz(mat){
+    let saida = ''
+    for(let i=0; i<mat.lenght; i++){
+        saida = saida + '\n'
+        for(let j=0; j<mat[i].lenght; j++){
+            saida = saida + " " + mat[i][j]
+        }
+    }
+    alert(saida)
+}
+function exe1(){
+    let mat = []
+    entrada(mat)
+    alert(`A soma dos números entre 15 e 20 é ${somaEntre15e20(mat)}.`)
+    exibeMatriz(mat)
+}
+exe1()
