552580d1f96a923b993afc8b518f17837d351e14
DevKunoichi <andresa.paula1981@gmail.com>
2025-06-14 16:08:27 -0300

    Exercícios de Matriz

--- Lista7/js/scripts.js
+++ Lista7/js/scripts.js
@@ -1,6 +1,5 @@
 //exemplo1
-function entrada(mat){
-    //será 3 x 3
+function entrada(mat){ //será 3 x 3
     for(let i=0; i<3; i++){
         mat[i] = [] //cria cada linha da matriz
         for(let j=0; j<3; j++){
@@ -10,14 +9,14 @@ function entrada(mat){
 }
 function calculaSomaDiagonalPrincipal(mat){
     let soma = 0
-    for(let i=0; i<3; i++){
-        for(let j=0; j<3; j++){
+    for(let i=0; i<3; i++){ //para cada linha
+        for(let j=0; j<3; j++){ //para cada coluna
            if (i == j){
                 soma += mat[i][j]
             }
         }
     }
-    return soma
+    return soma //função vai retornar a soma
 }
 function exemplo(){
     let mat = []
@@ -27,11 +26,11 @@ function exemplo(){
 exemplo()
 
 //exercício1
-function exe1(mat){
+function entrada(mat){
     for (let i=0; i<3; i++){
          mat[i] = []
          for(let j=0; j<3; j++){
-            mat[i][j] = parseInt(Math.random() * 10) + 15 //gera número aletório 
+            mat[i][j] = parseInt(Math.random() * 10) + 15 //gera número aletório de 15 a 24
          }
     }
 }
@@ -48,9 +47,9 @@ function somaEntre15e20(mat){
 }
 function exibeMatriz(mat){
     let saida = ''
-    for(let i=0; i<mat.lenght; i++){
+    for(let i=0; i<mat.length; i++){
         saida = saida + '\n'
-        for(let j=0; j<mat[i].lenght; j++){
+        for(let j=0; j<mat[i].length; j++){
             saida = saida + " " + mat[i][j]
         }
     }
@@ -63,4 +62,99 @@ function exe1(){
     exibeMatriz(mat)
 }
 exe1()
+
+//exercício2
+function entrada(mat){
+    for(let i=0; i<2; i++){
+        mat [i] = []
+        for(let j=0; j<4; j++){
+            mat[i][j] = Math.floor(Math.random() * 21) //gera números entre 0 e 20
+        }
+    }
+}
+function contaElementosEntre12e20(mat){
+    for (let i=0; i<2; i++){ 
+        let conta = 0 //inicializa o contador para cada linha
+        for (let j=0; j<4; j++){ 
+            if(mat[i][j] >= 12 && mat[i][j] <= 20){
+                conta++
+            }
+        }
+        alert(`A quantidade de elementos entre 12 e 20 na linha ${i} é ${conta}.`)
+    }
+}
+function exibeMatriz(mat){
+    let saida = ''
+    for(let i=0; i<mat.length; i++){
+        saida = saida + '\n'
+        for(let j=0; j<mat[i].length; j++){
+            saida = saida + " " + mat[i][j]
+        }
+    }
+    alert(saida)
+}
+function mediaElementosPares(mat){
+    let soma = 0
+    let qtde = 0
+    for(let i=0; i<mat.length; i++){
+        for(let j=0; j<mat[i].length; j++){
+            if(mat[i][j] % 2 == 0){
+                soma = soma + mat[i][j]
+                qtde++
+            }
+        }   
+    }
+    return soma / qtde 
+}
+function exe2(){
+    let mat = []
+    entrada(mat)
+    exibeMatriz(mat)
+    contaElementosEntre12e20(mat)
+    mediaElementosPares(mat)
+    alert(`A média dos elementos pares é ${mediaElementosPares(mat)}.`)
+}
+exe2()
+
+//exercício3
+function entrada(mat){
+    for(let i=0; i<6; i++){
+        mat[i] = []
+        for(let j=0; j<3; j++){
+            mat[i][j] = Math.floor(Math.random() * 21)
+        }
+    }
+}
+function calculaMaiorMenor(mat){
+    //inicia a variável maior e menor como sendo o 1° elemento 
+    let maior = mat[0][0], menor = mat[0][0]
+    let iMaior = 0, jMaior = 0, iMenor = 0, jMenor = 0
+    //busca por elementos maiores e menores
+    for(let i=0; i<6; i++){
+        for(let j=0; j<3; j++){
+            if(mat[i][j] > maior){
+               maior = mat[i][j]
+               iMaior = i
+               jMaior = j
+            }
+            if(mat[i][j] < menor){
+                menor = mat[i][j]
+                iMenor = i
+                jMenor = j 
+            }
+        }
+    }
+    alert(mat)
+    alert(`O maior ${maior} está na posição [${iMaior}][${jMaior}], e o menor ${menor} está na posição [${iMenor}][${jMenor}].`)
+}
+function exe3(){
+    let mat = []
+    entrada(mat)
+    calculaMaiorMenor(mat)
+}
+exe3()
+
+
+
+
 
